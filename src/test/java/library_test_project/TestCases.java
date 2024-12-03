c759c1ad4d5061f944a43967461c7072a5096104
Caiazza71 <82773998+Caiazza71@users.noreply.github.com>
2024-12-02 21:34:13 -0500

    refactor name

--- src/test/java/library_test_project/TestCases.java
+++ src/test/java/library_test_project/TestCases.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-public class BookTest {
+public class TestCases {
 
 	@Test
 	public void bookTest() {
