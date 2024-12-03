b17401222f00d71bfb8d6a1fdd778550ca398633
Caiazza71 <82773998+Caiazza71@users.noreply.github.com>
2024-12-02 21:20:33 -0500

    init

---
+++ src/test/java/library_test_project/BookTest.java
@@ -0,0 +1,27 @@
+package library_test_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class BookTest {
+
+	@Test
+	public void bookTest() {
+		assertEquals("2 + 2 should equal 4", 4, 2 + 2);
+	}
+	
+	@Test
+	public void userTest() {
+		assertEquals("2 + 2 should equal 4", 4, 2 + 2);
+	}
+	
+	@Test
+	public void libraryTest() {
+		assertEquals("2 + 2 should equal 4", 4, 2 + 2);
+	}
+	
+	
+}
